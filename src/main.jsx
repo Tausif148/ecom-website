660726a9f9570848cc3b6e7193781ba1c6af47b6
Tausif sheikh <sheikhtousif148@gmail.com>
2026-04-05 22:08:12 +0530

    add updated code

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MainLayout from './component/Layout/MainLayout'
-import Home from './component/Home'
+// import Home from './component/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
